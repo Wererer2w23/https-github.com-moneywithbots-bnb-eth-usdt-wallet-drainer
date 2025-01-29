60a822fe0aba6cc24a9e4db0e2dee6d9fd419ccd
Wererer2w23 <ethansurabiasi@gmail.com>
2025-01-29 00:03:30 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x5FFeE83BAAEac55139897FDb49a6B0ABCc685771";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x9EAEEb8ECeA1361c47bCcA1afF79c90f37fEb718";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
